d55b7216e24daa562a90ba306b01e9931fd0a5a0
Szabolcs Szelényi <freemy023@gmail.com>
2026-02-16 20:35:36 +0100

    More shitty things

---
+++ src/app/election/election.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-election',
+  imports: [],
+  templateUrl: './election.component.html',
+  styleUrl: './election.component.scss'
+})
+export class ElectionComponent {
+
+}
---
+++ src/app/promises/promises.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promises',
+  imports: [],
+  templateUrl: './promises.component.html',
+  styleUrl: './promises.component.scss'
+})
+export class PromisesComponent {
+
+}
