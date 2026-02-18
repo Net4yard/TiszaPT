ad8fabd213e80b2e93823e49e38df7b4566cd674
dev_Szabi <szabolcs.szelenyi23@gmail.com>
2026-02-18 15:01:12 +0100

    Small changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ElectionComponent } from './election/election.component';
-import { PromisesComponent } from './promises/promises.component';
 import { StatsComponent } from './stats/stats.component';
 
 export const routes: Routes = [
 	{ path: '', component: MainComponent },
 	{ path: 'election', component: ElectionComponent },
-	{ path: 'promises', component: PromisesComponent },
 	{ path: 'stats', component: StatsComponent },
 	{ path: '**', redirectTo: '' }
 ];
